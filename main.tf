bb5aea425775cb83fef4b1cc7149a529bf47f2ea
saikatghosh1998 <saikatghosh449@gmail.com>
2022-09-15 15:04:38 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+#Creating resource group
+resource "azurerm_resource_group" "aks-rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+#Assigning role to AKS for pulling image from acr
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
+
+#Creating ACR
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+#Creating AKS
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = "kubenet" 
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "saikat_test_rg"
+location            = "Centralindia"
+cluster_name        = "saikat-test-aks"
+kubernetes_version  = "1.22.6"
+system_node_count   = 1
+acr_name            = "saikattestacr"
